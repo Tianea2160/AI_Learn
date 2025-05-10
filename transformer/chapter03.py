dbf50a0c2c41792002057ecfba1c1a571139d2b3
johyeonjun <rhlehfndvkd7557@daum.net>
2025-05-10 11:42:52 +0900

    트랜스포머 encoder, decoder 실습

--- transformer/chapter03.py
+++ transformer/chapter03.py
@@ -1,14 +1,18 @@
 from math import sqrt
 
 import torch
-from torch.functional import F
 from torch import nn
+from torch.functional import F
 from transformers import AutoConfig, AutoTokenizer
 
 
 def scaled_dot_product_attention(query, key, value):
     dim_k = query.size(-1)
     scores = torch.bmm(query, key.transpose(1, 2)) / sqrt(dim_k)
+
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, float("-inf"))
+
     weights = F.softmax(scores, dim=-1)
     return torch.bmm(weights, value)
 
@@ -57,18 +61,79 @@ class FeedForward(nn.Module):
         return x
 
 
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.layer_norm_1 = nn.LayerNorm(config.hidden_size)
+        self.layer_norm_2 = nn.LayerNorm(config.hidden_size)
+        self.attention = MultiHeadAttention(config)
+        self.feed_forward = FeedForward(config)
+
+    def forward(self, x):
+        hidden_state = self.layer_norm_1(x)
+        x = x + self.attention(hidden_state)
+
+        x = x + self.feed_forward(self.layer_norm_2(x))
+        return x
+
+
+class Embeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.layer_norm = nn.LayerNorm(config.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout()
+
+    def forward(self, input_ids):
+        seq_length = input_ids.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long).unsqueeze(0)
+        token_embeddings = self.token_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings = token_embeddings + position_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embeddings = Embeddings(config)
+        self.layers = nn.ModuleList([TransformerEncoderLayer(config) for _ in range(config.num_hidden_layers)])
+
+    def forward(self, x):
+        x = self.embeddings(x)
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class TransformerForSequenceClassification(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = TransformerEncoder(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, x):
+        x = self.encoder(x)[:, 0, :]
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+
+
 model_ckpt = "bert-base-uncased"
 text = "time files like an arrow"
 
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 config = AutoConfig.from_pretrained(model_ckpt, output_attiontoins=True)
-multi_head_attention = MultiHeadAttention(config)
-
 inputs = tokenizer(text, return_tensors='pt', add_special_tokens=False)
-token_emb = nn.Embedding(config.vocab_size, config.hidden_size)
 
-input_emb = token_emb(inputs.input_ids)
-print(input_emb.size())
+seq_len = inputs.input_ids.size(-1)
+mask = torch.tril(torch.ones(seq_len, seq_len)).unsqueeze(0)
 
-attn_output = multi_head_attention(input_emb)
-print(attn_output.size())
+config.num_labels = 3
+encoder_classifier = TransformerForSequenceClassification(config)
+print(encoder_classifier(inputs.input_ids).size())
